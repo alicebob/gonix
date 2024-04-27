a9c6840b97404962c8f992f263b497e9dd9bc2c1
Harmen <alicebob@lijzij.de>
2024-04-27 09:19:43 +0200

    working flake

--- Makefile
+++ Makefile
@@ -1,4 +1,12 @@
+.PHONY: build
+build:
+	nix build
+
 .PHONY: vendor
 vendor:
 	go mod tidy
 	go mod vendor
+
+.PHONY: fmt
+fmt:
+	nix fmt flake.nix
---
+++ flake.nix
@@ -0,0 +1,59 @@
+{
+  description = "compile test for Go";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    (flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        everything = pkgs.buildGoModule {
+          name = "gonix";
+          src = pkgs.lib.sourceByRegex ./. [
+            "^go.(mod|sum)$"
+            "vendor"
+            "vendor/.*"
+            "echo-svc"
+            "echo-svc/.*"
+          ];
+          vendorHash = null; # uses ./vendor/
+          checkPhase = "true"; # no unittests please
+          #postInstall = ''
+          #mkdir -p $out/run/
+          #'';
+        };
+      in
+      {
+        packages.default = pkgs.stdenv.mkDerivation {
+          name = "echo";
+          src = everything;
+          configurePhase = "true";
+          buildPhase = "true";
+          installPhase = ''
+            mkdir -p $out/;
+            cp bin/echo-svc $out/;
+          '';
+        };
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            go
+            #(sox.override { enableLame = true; })
+            #opusTools
+            #ffmpeg-headless
+            #python310Packages.weasyprint
+            # gotools
+            # go-tools
+          ];
+        };
+        formatter = pkgs.nixfmt-rfc-style;
+      }
+    ));
+}
